4926a16c26da28b4632341c3071afc33b41a7aa2
arifvtti <arif.vtti@gmail.com>
2022-12-21 03:38:28 +0600

    conditionaly render in html

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ app.set('view engine', 'ejs');
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'EJS is an awesome template engine for Express' });
+
+    const post = {
+        title: 'This is test title',
+        body: 'This is body',
+        published: true
+    }
+
+    res.render('index', { title: 'EJS is an awesome template engine for Express', post });
 })
 
 app.listen(port, () => {
